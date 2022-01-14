69b6457d0371023d7de9f0b65398c79c9a1d5cc2
ariesdimasy <ariesdimasy@gmail.com>
2022-01-14 10:09:05 +0700

    update release feature

--- routes/mypokemons.js
+++ routes/mypokemons.js
@@ -35,6 +35,32 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/detail/:_id", async (req, res) => {
+  const { _id } = req.params;
+
+  let myPokemon;
+
+  try {
+    myPokemon = await Mypokemon.findOne({
+      _id: _id,
+    });
+
+    if (myPokemon === null) {
+      res.status(404).json({ message: "Cannot Find Your pokemon" });
+    } else {
+      res.status(200).json({
+        data: myPokemon,
+        message: "you sucessfully get one pokemon data",
+      });
+    }
+  } catch (err) {
+    console.log(" err => ", err);
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+});
+
 router.post("/catch", async (req, res) => {
   const probability = [true, false];
   const chance = get_random(probability);
@@ -71,8 +97,10 @@ router.post("/catch", async (req, res) => {
 
 router.post("/changename/:id", async (req, res) => {});
 
-router.delete("/release/:id", async (req, res) => {
+router.delete("/release/:_id", async (req, res) => {
   const isRelease = primeNumber(random_number());
+
+  // console.log(req.params);
 
   if (isRelease) {
     try {
@@ -82,13 +110,18 @@ router.delete("/release/:id", async (req, res) => {
 
       res.status(201).json({
         message: "you sucessfully release this pokemon",
+        success: true,
       });
     } catch (err) {
-      res.status(400).json(err.message);
+      res.status(400).json({
+        message: err.message,
+        success: false,
+      });
     }
   } else {
     res.status(201).json({
       message: "you cant release this pokemon, try again",
+      success: false,
     });
   }
 });
